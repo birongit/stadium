43310b140bde8b6c27fbd149552416316a680581
birongit <birongit@gmail.com>
2022-06-09 16:16:19 -0700

    Fix tests and coverage

--- test_game.py
+++ test_game.py
@@ -14,9 +14,14 @@ def test_game():
     # These calls are not allowed
     with pytest.raises(AssertionError):
         game.step(None, None)
+    with pytest.raises(AssertionError):
         game.step(None, 0)
+    with pytest.raises(AssertionError):
+        game.step(Action.STAY, None)
+    with pytest.raises(AssertionError):
         game.step(Action.STAY, -1)
-        game.step(Action.Stay, num_agents)
+    with pytest.raises(AssertionError):
+        game.step(Action.STAY, num_agents)
 
     # These calls are allowed
     [game.step(Action.STAY, agent) for agent in range(num_agents)]
